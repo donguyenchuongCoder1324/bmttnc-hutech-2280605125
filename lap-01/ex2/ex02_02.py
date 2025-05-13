a2695f5fcd0aa44fe74eb5d5606183c8e3fd3897
NgChuong1324 <donguyenchuong010304@gmail.com>
2025-05-13 16:18:41 +0700

    ex2

---
+++ lap-01/ex2/ex02_02.py
@@ -0,0 +1,6 @@
+# Nhập bán kính từ người dùng
+ban_kinh = float(input("Nhập vào bán kính của hình tròn: "))
+# Tính diện tích của hình tròn
+dien_tich = 3.14 * (ban_kinh ** 2)
+# In diẹn tích hình tròn ra màng hình
+print("Diện tích hình tròn của bạn là: ", dien_tich)
